055a433b66c98edf559cf7846e120a75713e8ced
YongSSYongSS <130008506+YongSSYongSS@users.noreply.github.com>
2025-03-30 22:50:26 +0900

    Add files via upload

---
+++ 백준_입출력과사칙연산.py
@@ -0,0 +1,93 @@
+"""
+1000.
+A,B = input().split()
+print(int(A) + int(B))
+
+1001.
+A, B = input().split()
+print(int(A)-int(B))
+
+10998.
+A, B = input().split()
+print(int(A)*int(B))
+
+1008
+A,B = input().split()
+print(int(A)/int(B))"
+
+10869
+A, B= input().split()
+print(int(A)+int(B))
+print(int(A)-int(B))
+print(int(A)*int(B))
+print(int(A)//int(B))
+print(int(A)%int(B))
+
+
+10926
+A = input()
+print(str(A)+"??!")
+
+18108
+y = int(input())
+
+print(y-543)
+
+
+10430
+A,B,C = map(int,input().split())
+print((A+B)%C)
+print(((A%C)+(B%C))%C)
+print((A*B)%C)
+print(((A%C)*(B%C))%C)
+
+2588
+-------------내 답안-------------
+A = int(input())
+B = int(input())
+
+print((B%10)*A)
+print((B//10%10)*A)
+print((B//100)*A)
+print(A*B)
+----------------------------------
+
+------------인터넷답---------------
+A = int(input())
+B = input()
+
+print(A*int(B[2]))
+print(A*int(B[1]))
+print(A*int(B[0]))
+print(A*int(B))
+----------------------------------
+
+
+-----------궁금------------
+A = int(input())
+
+print(A)
+print(type(A))
+print(A[2])
+
+'int' object is not subscriptable  -> 정수형은 인덱싱 및 슬라이싱이 안되나 보다 
+
+
+11382
+A,B,C = map(int, input().split())
+print(A+B+C)
+
+10171
+print("\    /\\")
+print(" )  ( ')")
+print("(  /  ) ")
+print(" \(__)| ")"
+
+10172
+print("|\\_/|")
+print("|q p|   /}")
+print('( 0 )\"\"\"\\')
+print('|"^"`    |')
+print("||_/=\\\\__|")
+
+"""
